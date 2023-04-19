fde3ce74e398f761e30780e8ea14d4bede27373d
Dheeraj3018 <vsdheeraj17@gmail.com>
2023-04-19 17:23:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const countdown = document.querySelector('.countdown')
+let num = 10;
+console.log(countdown.innerHTML)
+
+setTimeout(() => {
+    countdown.innerHTML = --num
+    console.log(countdown.innerHTML)
+    setTimeout(() => {
+        countdown.innerHTML = --num
+        console.log(countdown.innerHTML)
+        setTimeout(() => {
+            countdown.innerHTML = --num
+            console.log(countdown.innerHTML)
+            setTimeout(() => {
+                countdown.innerHTML = --num
+                console.log(countdown.innerHTML)
+                setTimeout(() => {
+                    countdown.innerHTML = --num
+                    console.log(countdown.innerHTML)
+                    setTimeout(() => {
+                        countdown.innerHTML = --num
+                        console.log(countdown.innerHTML)
+                        setTimeout(() => {
+                            countdown.innerHTML = --num
+                            console.log(countdown.innerHTML)
+                            setTimeout(() => {
+                                countdown.innerHTML = --num
+                                console.log(countdown.innerHTML)
+                                setTimeout(() => {
+                                    countdown.innerHTML = --num
+                                    console.log(countdown.innerHTML)
+                                    setTimeout(() => {
+                                        countdown.innerHTML = " Happy Independence day💐💐💐💐"
+                                        console.log(countdown.innerHTML)
+                                     
+                                                                                                                           
+                                                                              
+                                   
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
+},1000)
